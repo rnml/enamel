4718a01beaa673ee7049746fbafdd41871e447a2
nathanml <none@none>
2012-10-28 00:13:50 -0400

    driver

--- z.sh
+++ z.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-function app {
-  echo "(fix (app (mod $1) (mod $2)))"
-}
-
-function pair {
-  app "$(app "(name pair)" "$1")" "$2"
-}
+function app  { echo "(fix (app (mod $1) (mod $2)))"; }
+function pair { app "$(app "(name pair)" "$1")" "$2"; }
+function fun  { app "$(app "(name fun)" "$1")" "$2"; }
 
 ./main.exe <<EOF
-$(pair "(name int)" "(name int)")
+$(fun "(name int)" "(name int)")
 EOF
