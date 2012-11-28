62b1170cf712659c6014ca4489b2635bb92a1925
nathanml <none@none>
2012-11-28 00:56:30 -0500

    more committed constant hash-consing

--- constant.ml
+++ constant.ml
@@ -6,5 +6,7 @@ let store : t String.Table.t = String.Table.create ~size:100 ()
 
 let of_string x = Table.find_or_add store x ~default:(fun () -> x)
 
+let t_of_sexp s = of_string (t_of_sexp s)
+
 let pretty = Pretty.text
 
