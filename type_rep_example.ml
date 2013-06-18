6a614c9b85145bce7659c94891647ea14afd0fa3
rnml <none@none>
2013-06-17 21:30:23 -0400

    mutually recursive example

--- type_rep_example.ml
+++ type_rep_example.ml
@@ -1,7 +1,10 @@
 module Sound = struct
-  type t = Roar | Meow of int | Bark of string * int
+  type t =
+    | Roar
+    | Meow of int
+    | Bark of string * int
   let type_rep =
-    let module T = struct
+    Type.Rep.Variant (module struct
       type nonrec t = t
       let name : t Type.Name.t = Type.Name.create ~name:"Sound.t"
       module Label = struct
@@ -20,26 +23,19 @@ module Sound = struct
         type univ = Label : 'a t -> univ
         let all = [Label Roar; Label Bark; Label Meow]
       end
-
       type 'a tag = 'a Label.t
-
       type rep = Tagged : 'a tag * 'a -> rep
-
       let project = function
         | Roar        -> Tagged (Label.Roar, ())
         | Meow a      -> Tagged (Label.Meow, a)
         | Bark (a, b) -> Tagged (Label.Bark, (a, b))
-
       let put (type a) (tag : a tag) (arg : a) : t =
         match (tag, arg) with
         | (Label.Roar, ())     -> Roar
         | (Label.Meow, a)      -> Meow a
         | (Label.Bark, (a, b)) -> Bark (a, b)
-
       let inject = fun (Tagged (tag, arg)) -> put tag arg
-
-    end in
-    Type.Rep.Variant (module T : Type.Rep.Variant.T with type t = t)
+    end : Type.Rep.Variant.T with type t = t)
 end
 
 module Animal = struct
@@ -49,7 +45,7 @@ module Animal = struct
     size : int;
   }
   let type_rep =
-    let module T = struct
+    Type.Rep.Record (module struct
       type nonrec t = t
       let name : t Type.Name.t = Type.Name.create ~name:"Animal.t"
       module Label = struct
@@ -68,25 +64,89 @@ module Animal = struct
         type univ = Label : 'a t -> univ
         let all = [Label Name; Label Sound; Label Size]
       end
-
       type 'a field = 'a Label.t
-
       type rep = { lookup : 'a. 'a field -> 'a }
-
       let inject {lookup} = {
         name  = lookup Label.Name;
         sound = lookup Label.Sound;
         size  = lookup Label.Size;
       }
-
       let get (type a) (f : a field) (t:t) : a =
         match f with
         | Label.Name  -> t.name
         | Label.Sound -> t.sound
         | Label.Size  -> t.size
-
       let project r = { lookup = fun field -> get field r }
+    end : Type.Rep.Record.T with type t = t)
+end
 
-    end in
-    Type.Rep.Record (module T : Type.Rep.Record.T with type t = t)
+module Even_odd_lists = struct
+
+  type even =
+    | Nil
+    | Cons of int * odd
+  and odd = {
+    head : int;
+    tail : even;
+  }
+
+  let rec type_rep_of_even =
+    Type.Rep.Variant (module struct
+      type nonrec t = even
+      let name : t Type.Name.t = Type.Name.create ~name:"Sound.t"
+      module Label = struct
+        type 'a t =
+        | Nil : unit t
+        | Cons : (int * odd) t
+        let name_of : type a. a t -> string = function
+          | Nil -> "nil"
+          | Cons -> "cons"
+        let type_of : type a. a t -> a Type.Rep.t = function
+          | Nil -> Type.Rep.Unit
+          | Cons -> Type.Rep.(Pair (Int, type_rep_of_odd))
+        type univ = Label : 'a t -> univ
+        let all = [Label Nil; Label Cons]
+      end
+      type 'a tag = 'a Label.t
+      type rep = Tagged : 'a tag * 'a -> rep
+      let project = function
+        | Nil         -> Tagged (Label.Nil, ())
+        | Cons (a, b) -> Tagged (Label.Cons, (a, b))
+      let put (type a) (tag : a tag) (arg : a) : t =
+        match (tag, arg) with
+        | (Label.Nil,  ())     -> Nil
+        | (Label.Cons, (a, b)) -> Cons (a, b)
+      let inject = fun (Tagged (tag, arg)) -> put tag arg
+    end : Type.Rep.Variant.T with type t = even)
+
+  and type_rep_of_odd =
+    Type.Rep.Record (module struct
+      type nonrec t = odd
+      let name : t Type.Name.t = Type.Name.create ~name:"Animal.t"
+      module Label = struct
+        type 'a t =
+        | Head : int t
+        | Tail : even t
+        let name_of : type a. a t -> string = function
+          | Head -> "head"
+          | Tail -> "tail"
+        let type_of : type a. a t -> a Type.Rep.t = function
+          | Head -> Type.Rep.Int
+          | Tail -> type_rep_of_even
+        type univ = Label : 'a t -> univ
+        let all = [Label Head; Label Tail]
+      end
+      type 'a field = 'a Label.t
+      type rep = { lookup : 'a. 'a field -> 'a }
+      let inject {lookup} = {
+        head = lookup Label.Head;
+        tail = lookup Label.Tail;
+      }
+      let get (type a) (f : a field) (t:t) : a =
+        match f with
+        | Label.Head -> t.head
+        | Label.Tail -> t.tail
+      let project r = { lookup = fun field -> get field r }
+    end : Type.Rep.Record.T with type t = odd)
+
 end
--- type_rep_example.mli
+++ type_rep_example.mli
@@ -11,3 +11,15 @@ module Animal : sig
   }
   val type_rep : t Type.Rep.t
 end
+
+module Even_odd_lists : sig
+  type even =
+  | Nil
+  | Cons of int * odd
+  and odd = {
+    head : int;
+    tail : even;
+  }
+  val type_rep_of_even : even Type.Rep.t
+  val type_rep_of_odd : odd Type.Rep.t
+end
