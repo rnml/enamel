a805f41c9a0daae95abce842f74fd951076734ca
rnml <none@none>
2013-07-16 01:45:16 -0400

    type names for "unbound" type constructors

--- common.ml
+++ common.ml
@@ -3,20 +3,28 @@ module Bind = struct
   type ('pat, 'term) t = 'pat * 'term with sexp
   let create p t = (p, t)
   let unbind _ = failwith "Bind.unbind unimplemented"
+  module Type_name = Type.Name.Make2 (struct type nonrec ('p, 't) t = ('p, 't) t end)
+  let type_name = Type_name.lookup
 end
 
 module Rebind = struct
   type ('pat1, 'pat2) t = 'pat1 * 'pat2 with sexp
   let create p1 p2 = (p1, p2)
+  module Type_name = Type.Name.Make2 (struct type nonrec ('p1, 'p2) t = ('p1, 'p2) t end)
+  let type_name = Type_name.lookup
 end
 
 module Embed = struct
   type 'term t = 'term with sexp
   let create x = x
+  module Type_name = Type.Name.Make1 (struct type nonrec 't t = 't t end)
+  let type_name = Type_name.lookup
 end
 
 module Rec = struct
   type 'pat t = 'pat with sexp
   let create x = x
+  module Type_name = Type.Name.Make1 (struct type nonrec 'p t = 'p t end)
+  let type_name = Type_name.lookup
 end
 
--- common.mli
+++ common.mli
@@ -3,20 +3,24 @@ module Bind : sig
   type ('pat, 'term) t
   val create : 'pat -> 'term -> ('pat, 'term) t
   val unbind : ('pat, 'term) t -> 'pat * 'term
+  val type_name : 'pat Type.Name.t -> 'term Type.Name.t -> ('pat, 'term) t Type.Name.t
 end
 
 module Rebind : sig
   type ('pat1, 'pat2) t = private 'pat1 * 'pat2
   val create : 'pat1 -> 'pat2 -> ('pat1, 'pat2) t
+  val type_name : 'pat1 Type.Name.t -> 'pat2 Type.Name.t -> ('pat1, 'pat2) t Type.Name.t
 end
 
 module Embed : sig
   type 'term t = private 'term
   val create : 'term -> 'term t
+  val type_name : 'term Type.Name.t -> 'term t Type.Name.t
 end
 
 module Rec : sig
   type 'pat t = private 'pat
   val create : 'pat -> 'pat t
+  val type_name : 'pat Type.Name.t -> 'pat t Type.Name.t
 end
 
