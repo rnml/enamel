ed6e36b0ccb96cc05a34a7fbef4563fe3571c396
rnml <none@none>
2013-08-12 22:35:22 -0400

    fixed recursive Type

--- typerep/type.ml
+++ typerep/type.ml
@@ -114,7 +114,61 @@ module rec Rep : sig
 
 end = struct
 
-  include Rep
+  type 'a t =
+  | Int : int t
+  | Char : char t
+  | Float : float t
+  | String : string t
+  | Bool : bool t
+  | Unit : unit t
+  | Option : 'a t -> 'a option t
+  | List : 'a t -> 'a list t
+  | Lazy : 'a t -> 'a Lazy.t t
+  | Pair : 'a t * 'b t -> ('a * 'b) t
+  | Triple : 'a t * 'b t * 'c t -> ('a * 'b * 'c) t
+  | Record of 'a Rep.Record.t
+  | Variant of 'a Rep.Variant.t
+  | Abstract of 'a Name.t
+
+  module type Labeled = sig
+    type t
+    val name : t Name.t
+    module Label : sig
+      type 'a t
+      val name_of : 'a t -> string
+      val type_of : 'a t -> 'a Rep.t
+      type univ = Label : 'a t -> univ
+      val all : univ list
+    end
+    type rep
+    val inject : rep -> t
+    val project : t -> rep
+  end
+
+  module Record = struct
+    module type T = sig
+      type 'a field
+      type rep = { lookup : 'a. 'a field -> 'a }
+      include Labeled
+      with type 'a Label.t = 'a field
+      and type rep := rep
+      val get : 'a field -> t -> 'a
+    end
+    type 'a t = (module T with type t = 'a)
+  end
+
+  module Variant = struct
+    module type T = sig
+      type 'a tag
+      type rep = Tagged : 'a tag * 'a -> rep
+      include Labeled
+      with type 'a Label.t = 'a tag
+      and type rep := rep
+      val put : 'a tag -> 'a -> t
+    end
+    type 'a t = (module T with type t = 'a)
+  end
+
 
   module Id = struct
 
@@ -140,6 +194,8 @@ end = struct
       | Pair   (a, b)    -> Pair_name.lookup (id a) (id b)
       | Triple (a, b, c) -> Triple_name.lookup (id a) (id b) (id c)
   end
+
+  let id = Id.id
 
   let rec same t1 t2 = Result.ok (Name.same_witness (id t1) (id t2))
 
