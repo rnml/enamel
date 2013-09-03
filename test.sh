85b939b7dc1d85cf73bebac59c5ff4bb083fda0a
rnml <none@none>
2013-09-03 00:25:31 -0400

    another test case

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 'checking an F type ...'
+./main.exe check-f-type <<EOF
+(Forall ((a *)) (Fun a a))
+EOF
+
+echo 'checking an F term ...'
+./main.exe check-f-term <<EOF
+(Fun ((Type a *) (x a)) x)
+EOF
+
+echo 'checking an F term ...'
+./main.exe check-f-term <<EOF
+(Pack
+(Forall ((a *)) (Fun a a))
+(Fun ((Type a *) (x a)) x)
+:
+Exists a . a
+)
+EOF
