231e37d27db3ad125f4a23bd88410623eb62f782
rnml <none@none>
2013-09-05 01:22:50 -0400

    formatting

--- module_system.ml
+++ module_system.ml
@@ -13,14 +13,12 @@ module type T = sig
   end
   module Ty : sig
     type 'a t with sexp
-    type 'a check =
-        Target.Context.t -> 'a -> F.Ty.t * F.Kind.t
+    type 'a check = Target.Context.t -> 'a -> F.Ty.t * F.Kind.t
     val ok : 'a check -> 'a t check
   end
   module Tm : sig
     type ('a, 'b) t with sexp
-    type 'b check =
-        Target.Context.t -> 'b -> F.Tm.t * F.Ty.t
+    type 'b check = Target.Context.t -> 'b -> F.Tm.t * F.Ty.t
     val ok : 'a Ty.check -> 'b check -> ('a, 'b) t check
   end
 end
