003666642f67945bfe8fe487319e1b563214f282
rnml <none@none>
2015-03-09 23:11:19 -0400

    started

--- syntax0.mli
+++ syntax0.mli
@@ -109,3 +109,81 @@ module Target : sig
   end
 
 end
+
+module type Uterm = sig
+  type t
+  val tc : t Unbound_lib.Unbound.Term_tc.t
+end
+
+module Fing_modules
+         (Base : functor (Path : Term) -> sig
+            module Kind : sig type t end
+            module Type : sig type t end
+            module Term : sig type t end
+          end) : sig
+
+  module Label : Identifiable
+  module Name : Identifiable
+
+  module rec Type : sig
+    type t =
+      | Wrap of t Base.Type.t
+      | Path of Module.t
+      (* | Let  of Struct.t * t *)
+  end
+
+  and Term : sig
+    type t =
+      | Wrap of (Type.t, t) Base.Term.t
+      | Path of Module.t
+      (* | Let  of Struct.t * t *)
+  end
+
+  and Signature : sig
+    type t =
+      | Path      of Module.t
+      | Val       of Type.t
+      | Type      of Type.t
+      | Abstype   of Base.Kind.t
+      | Signature of t
+      | Struct    of Decl.t
+      | Fun       of Name.t * t * t
+      | Where     of t * Label.t list * Type.t
+      (* | Let     of Struct.t * t *)
+  end
+
+  and Decl : sig
+    type t =
+      | Decl    of Name.t * Signature.t
+      | Nil
+      | Cat     of t * t
+      | Include of Signature.t
+      (* | Local   of Struct.t * t *)
+  end
+
+  and Module : sig
+    type t =
+      | Name      of Name.t
+      | Type      of Type.t
+      | Value     of Term.t
+      | Struct    of Struct.t
+      | Signature of Signature.t
+      | Dot       of t * Label.t
+      | Fun       of Name.t * Signature.t * t
+      | AppV      of Name.t * Name.t
+      | SealV     of Name.t * Signature.t
+      (* | App    of t * t *)
+      (* | Seal   of t * Signature.t *)
+      (* | Let    of Struct.t * t *)
+  end
+
+  and Struct : sig
+    type t =
+      | Let     of Name.t * Module.t
+      | Nil
+      | Cat     of t * t
+      | Include of Module.t
+      (* | Local   of t * t *)
+  end
+
+end
